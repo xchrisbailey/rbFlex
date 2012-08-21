9ce5406300d420cf9398dddf05dc8af74ce99989
chris bailey <chris@subcu.lt>
2012-08-21 02:33:08 -0400

    code prep

--- lib/rbFlex.rb
+++ lib/rbFlex.rb
@@ -1,4 +1,10 @@
 require "rbFlex/version"
+require 'net/ssh'
+require 'action_view'
+include ActionView::Helpers::DateHelper
+require 'active_support/all'
+require 'slop'
+require 'date'
 
 module RbFlex
   # Your code goes here...
